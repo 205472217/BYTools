a593157c62a3c5a5dae22c18f914fa78d50ef400
baiyin <205472217@qq.com>
2026-07-01 15:33:21 +0800

    临时删除plugins所有

--- plugins/common/SettingsHelper.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <QSettings>
-#include <QString>
-
-/// 返回指定 group 名的 QSettings 引用（INI 文件，缓存，线程安全）
-/// 例：QSettings &s = pluginGroupSettings(MyPlugin::kIniSection);
-QSettings &pluginGroupSettings(const char *groupName);
